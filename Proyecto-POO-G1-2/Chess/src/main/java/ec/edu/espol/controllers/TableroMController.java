8db15692c97025139191aab70f3d269699b69c8d
Jonthz <jnzambra@fiec.espol.edu.ec>
2023-08-21 11:51:09 -0500

    a

--- Proyecto-POO-G1-2/Chess/src/main/java/ec/edu/espol/controllers/TableroMController.java
+++ Proyecto-POO-G1-2/Chess/src/main/java/ec/edu/espol/controllers/TableroMController.java
@@ -6,11 +6,13 @@ package ec.edu.espol.controllers;
 
 import java.net.URL;
 import java.util.ResourceBundle;
+import javafx.fxml.FXML;
 import javafx.fxml.Initializable;
 import javafx.geometry.Insets;
 import javafx.scene.control.Button;
 import javafx.scene.image.Image;
 import javafx.scene.image.ImageView;
+import javafx.scene.input.MouseEvent;
 import javafx.scene.layout.GridPane;
 
 /**
@@ -23,41 +25,62 @@ public class TableroMController implements Initializable {
     /**
      * Initializes the controller class.
      */
+    @FXML
+    private ImageView selectedPiece; // Almacena la pieza seleccionada
+    private int currentRow, currentCol; // Almacena la ubicación actual de la pieza
+    private GridPane gridpane;
+
     @Override
     public void initialize(URL url, ResourceBundle rb) {
-        GridPane gridPane = new GridPane();
-        gridPane.setPadding(new Insets(10));
-        gridPane.setHgap(5);
-        gridPane.setVgap(5);
+        int numRows = 8;
+        int numCols = 8;
 
-        int numRows = 8; // Número de filas en la matriz
-        int numCols = 8; // Número de columnas en la matriz
+        Button[][] buttons = new Button[numRows][numCols]; // Inicializa la matriz de botones
 
-        // Crear una matriz de botones
-        Button[][] buttons = new Button[numRows][numCols];
-        
-        
-//        for (int i = 0; i < numRows; i++) {
-//            for (int j = 0; j < numCols; j++) {
-//                if(i==0 || i==1 || i==6 || i==7 ){
-//                    Image img = new Image("fichas/logo.png");
-//                    ImageView view = new ImageView(img);
-//                    view.setFitHeight(80);
-//                    view.setPreserveRatio(true);
-//                    Button button = new Button();
-//                buttons[i][j] = button;
-//
-//                // Agregar el botón a la cuadrícula en la posición (i, j)
-//                gridPane.add(button, j, i);
-//
-//                }
-                
-                // Manejar eventos de clic en el botón
-//                button.setOnAction(event -> {
-//                    System.out.println("Botón clickeado: " + button.getText());
-//                });
+        // Configurar botones con imágenes para representar el tablero
+        for (int i = 0; i < numRows; i++) {
+            for (int j = 0; j < numCols; j++) {
+                if(i==1 || i ==0 || i==6 || i==7){
+                Button button = new Button();
+                button.setMinSize(50, 50); // Tamaño de los botones
+                button.setMaxSize(50, 50);
+
+                // Crear un ImageView y configurar una imagen para el botón
+                ImageView imageView = new ImageView(new Image("fichas/"+i+j+".png")); // Reemplaza con la ruta correcta
+
+                // Configurar clic para seleccionar pieza
+                button.setOnMouseClicked(e -> handlePieceClick(button.getOnMouseClicked()));
+
+                // Limpiar el contenido anterior del botón y agregar el ImageView
+                button.setGraphic(imageView);
+
+                // Agregar el botón a la cuadrícula en la posición (i, j)
+                gridPane.add(button, j, i);
+                buttons[i][j] = button; // Agregar el botón a la matriz
+            }
             }
         }
-    }    
-    
+    } 
+    @FXML
+    private void handlePieceClick(MouseEvent event) {
+        ImageView piece = (ImageView) event.getSource();
+        int row = GridPane.getRowIndex(piece);
+        int col = GridPane.getColumnIndex(piece);
+
+        // Implementa la lógica para mover las piezas según sea necesario
+        if (selectedPiece == null) {
+            selectedPiece = piece;
+            currentRow = row;
+            currentCol = col;
+        } else {
+            // Mueve la pieza seleccionada a la nueva ubicación
+            GridPane.setColumnIndex(selectedPiece, col);
+            GridPane.setRowIndex(selectedPiece, row);
+
+            // Limpia la selección
+            selectedPiece = null;
+        }
+    }
+
+        }
 }
