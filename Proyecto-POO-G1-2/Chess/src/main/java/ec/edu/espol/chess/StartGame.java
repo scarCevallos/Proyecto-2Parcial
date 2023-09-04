8f3b63371b05e94a0082b9c273363e3a21da7acf
Darvier88 <dpacheco@fiec.espol.edu.ec>
2023-09-04 14:18:44 -0500

    Fin por ahora

---
+++ Proyecto-POO-G1-2/Chess/src/main/java/ec/edu/espol/chess/StartGame.java
@@ -0,0 +1,15 @@
+/*
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Licenses/license-default.txt to change this license
+ * Click nbfs://nbhost/SystemFileSystem/Templates/Classes/Class.java to edit this template
+ */
+package ec.edu.espol.chess;
+
+/**
+ *
+ * @author HP
+ */
+public class StartGame {
+    public static void main(String[] args) {
+        App.main(args);
+    }
+}
